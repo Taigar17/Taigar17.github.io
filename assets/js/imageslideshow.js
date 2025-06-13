ffaa26c58886b67394638c563315f7ea3d3be41d
Taigar17 <akhussain145@gmail.com>
2025-06-13 15:36:00 +0100

    Added some more game images

--- assets/js/imageslideshow.js
+++ assets/js/imageslideshow.js
@@ -1,12 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   const imagePaths = [
+    "images/AgeofEmpires.png",
     "images/DiablioIV.png",
     "images/HogwartsLegacy.png",
     "images/MGS3.png",
     "images/ApexLegends.png",
     "images/CODMO3.png",
+    "images/ForzaMotorSport2023.png",
     "images/MetalGear.png",
-    "images/StreetFighter6.png"
+    "images/StreetFighter6.png",
+    "images/AgeofWonders4.png",
+    "images/F1Manager2023.png",
+    "images/Cyberpunk2077.png",
+    "images/MiraculousRiseofSphinx.png",
+    "images/Hotwheels2.png",
+    "images/Titanfall2.png"
   ];
 
   const slider = document.getElementById("imageSlider");
