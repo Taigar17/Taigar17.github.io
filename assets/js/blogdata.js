633c2b5a43880e8de33585ed319f579d6498be7b
Taigar17 <akhussain145@gmail.com>
2025-06-13 21:14:27 +0100

    First Version of Blogs

---
+++ assets/js/blogdata.js
@@ -0,0 +1,15 @@
+const blogs = [
+  {
+    id: 1,
+    title: "QA Automation Journey",
+    thumbnail: "images/blog_images/ADD HERE.png",
+    url: "assets/blogs/blog_pages/first-blog.html"
+  },
+  {
+    id: 2,
+    title: "CHANGE HERE",
+    thumbnail: "images/blog_images/ADD HERE.png",
+    url: "blogs/first-blog.html"
+  },
+  // Add more blogs...
+];
